7f62473b647420a279898ecc494feae508b99859
Hugo Lima <limabrot879@gmail.com>
2024-02-07 11:25:15 -0300

    Create JPA repository for tasks

---
+++ infrastructure/src/main/java/repositories/TaskRepository.java
@@ -0,0 +1,8 @@
+package repositories;
+
+import entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {}
